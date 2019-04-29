3af1035a4e6965fab1d6a7354d029bfc4d4895c3
dhanisjt <48464738+dhanisjt@users.noreply.github.com>
2019-04-29 11:29:10 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan krywn = new karyawan();
+
+            krywn.nama = "Dhani";
+            krywn.nik = "2351";
+            krywn.gaji = 1000000;
+
+            krywn.nama2 = "Andyan";
+            krywn.nik2 = "2323";
+            krywn.gaji2 = 2000000;
+
+            krywn.infoGaji();
+            Console.WriteLine();
+            krywn.kenaikanGaji();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
+
